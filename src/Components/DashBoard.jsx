2ed2b32a97d7c5147eda28c3bf3310d69638907b
Pallav Jain <jain.pallav1998gmail.com>
2022-03-10 01:52:19 +0530

    Deploying App

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -14,10 +14,10 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
 
   const factoryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
 
   async function ConnectWallet() {
     if (window.ethereum) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner();
       console.log("signer:", await signer.getAddress());
@@ -34,12 +34,12 @@ function App() {
   async function CreateFund(formData) {
     const { Target, Deadline, Description, Image } = formData;
     if (window.ethereum) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = await provider.getSigner();
       await provider.send("eth_requestAccounts", []);
       const FactoryData = new ethers.Contract(
         factoryAddress,
         factoryABI,
-        provider.getSigner()
+        signer
       );
 
       await FactoryData.CreateNewFund(Target, Deadline, Description, Image)
@@ -49,7 +49,6 @@ function App() {
   }
 
   async function getAllFund() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
     const FactoryData = new ethers.Contract(
       factoryAddress,
--- src/Components/FundDetails.jsx
+++ src/Components/FundDetails.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
+import styles from "./Styling.module.css";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LinearDeterminate from "./LinearProgress";
 
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -12,55 +13,52 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
 export default function FundDetails() {
   return (
-    <Box sx={{ flexGrow: 1, maxWidth: "80%", margin: "auto" }}>
+    <Box
+      sx={{ flexGrow: 1, maxWidth: "80%", margin: "auto", marginTop: "50px" }}
+    >
       <Grid container spacing={4}>
         <Grid item lg={4} md={6}>
           <Card>
             <CardActionArea>
               <CardMedia
                 component="img"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
+                height="180"
+                image="https://www.sciencealert.com/images/2020-06/processed/cancer_topic_1024.jpg"
                 alt="green iguana"
               />
               <CardContent>
-                <h3>Description</h3>
-                <p>
-                  <strong>ETH 10000</strong>&nbsp; raised out of ETH 1999999
+                <h2>Description</h2>
+                <p style={{ fontSize: "20px", textAlign: "left" }}>
+                  <strong>ETH 10000</strong> raised out of <br />
+                  <i>ETH 1999999</i>
                 </p>
-                <div>
+                <LinearDeterminate />
+                <div className={styles.donateflex}>
                   <div>
                     <AccessTimeIcon />
-                    10 Time Left
+                    &nbsp;
+                    <span>10 Time Left</span>
                   </div>
                   <div>
-                    <FavoriteIcon /> 10000 Supporters
+                    <FavoriteIcon />
+                    &nbsp;
+                    <span>10000 Supporters</span>
                   </div>
                 </div>
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
-                Donate
-              </Button>
+              <button className={styles.donatebutton}>Donate</button>
             </CardActions>
           </Card>
         </Grid>
         <Grid item lg={4} md={6}>
-          <Item>lg=6 md=4</Item>
+          <Paper>lg=6 md=4</Paper>
         </Grid>
         <Grid item lg={4} md={6}>
-          <Item>lg=6 md=4</Item>
+          <Paper>lg=6 md=4</Paper>
         </Grid>
       </Grid>
     </Box>
